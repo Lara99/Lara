f091d54a734376823c1aa03a790ebeadf67bf08a
Lara99 <45993014+Lara99@users.noreply.github.com>
2018-12-19 11:33:49 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -57,7 +57,13 @@
 			$num= rand (0,2);
 			sendFoto($chatId,$foto[$num],false, "La mia foto", $api);
 		}
-	
+	        if ($text=="audio"){
+			sendAudio($chatId,"audio.mp3",false,"Il mio Audio",$api);
+		}
+                if ($text=="testo"){
+			sendAudio($chatId,"testo.pdf",false,"Il mio documento",$api);
+		}
+			
 		
 		//aggiungo il comando di invio
 		//e lo invio
